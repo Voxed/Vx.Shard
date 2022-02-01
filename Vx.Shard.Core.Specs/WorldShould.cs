07c9ff3a25cef9e61a8c52742885969706b5598c
Petter Blomkvist <voxedg@gmail.com>
2022-02-01 01:20:20 +0100

    Added testing framework.

---
+++ Vx.Shard.Core.Specs/WorldShould.cs
@@ -0,0 +1,54 @@
+namespace Vx.Shard.Core.Specs;
+
+using System.Linq;
+using Xunit;
+
+public class WorldShould
+{
+    private class TestMessage : IMessage
+    {
+    }
+
+    private class TestComponent : IComponent
+    {
+    }
+
+    private readonly World _world;
+
+    public WorldShould()
+    {
+        _world = new World();
+    }
+
+    [Fact]
+    public void SendToMessageBus()
+    {
+        var messageReceived = false;
+        var builder = new MessageBusListenerBuilder();
+        builder.AddCallback<TestMessage>((_, _) => { messageReceived = true; });
+        _world.MessageBus.SetListener(builder.Build(_world));
+        _world.Send(new TestMessage());
+        Assert.True(messageReceived);
+    }
+
+    [Fact]
+    public void CreateUniqueEntities()
+    {
+        Assert.Equal(
+            10,
+            Enumerable.Range(0, 10).Select(_ => _world.CreateEntity().Id).Distinct().Count()
+        );
+    }
+
+    [Fact]
+    public void FindEntitiesWithComponent()
+    {
+        var entitiesWithComponent = Enumerable.Range(0, 10).Select(_ => _world.CreateEntity()).ToList();
+        entitiesWithComponent.ForEach(e => e.AddComponent(new TestComponent()));
+        Enumerable.Range(0, 10).ToList().ForEach(_ => _world.CreateEntity());
+        Assert.Equal(
+            entitiesWithComponent.ToList().Select(e => e.Id).OrderBy(e => e).ToList(),
+            _world.GetEntitiesWith<TestComponent>().ToList().Select(e => e.Id).OrderBy(e => e).ToList()
+            );
+    }
+}
