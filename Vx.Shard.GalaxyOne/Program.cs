a31f50834e7d876eebaeaec94e35b8bb5e152fb6
voxed <voxedg@gmail.com>
2022-03-11 02:07:23 +0100

    Created new demo

---
+++ Vx.Shard.GalaxyOne/Program.cs
@@ -0,0 +1,28 @@
+﻿using Vx.Shard.Common;
+using Vx.Shard.Core;
+using Vx.Shard.GalaxyOne;
+using Vx.Shard.Graphics;
+using Vx.Shard.Resources;
+using Vx.Shard.Sdl;
+using Vx.Shard.Window;
+
+var engine = new EngineBuilder()
+    // Add API abstractions, these systems register appropriate messages/component and set-up implementation
+    // independent logic.
+    .AddSystem(new SystemInput())
+    .AddSystem(new SystemGraphics())
+    .AddSystem(new SystemResources())
+    
+    // Add API implementations, this could be dynamic, choosing implementations based on OS.
+    .AddSystem(new SystemSdl())
+    .AddSystem(new SystemSdlGraphics())
+    .AddSystem(new SystemSdlInput())
+    
+    // Game specific systems.
+    .AddSystem(new SystemGalaxyOne())
+    .AddSystem(new SystemMovement())
+    
+    .AddSystem(new SystemMainLoop())
+    .Build();
+
+engine.Start();
